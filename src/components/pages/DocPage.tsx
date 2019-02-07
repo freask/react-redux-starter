557cdc6412420628854d0b1f48c6315ed04dd9f5
Kashin <kashin.a.ser@sberbank.ru>
2019-02-07 13:37:44 +0300

    test 1

---
+++ src/components/pages/DocPage.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+class DocPage extends React.Component {
+    render() {
+        const a = 5;
+        return (
+            <div>
+
+
+
+            </div>
+
+        )
+    }
+}
