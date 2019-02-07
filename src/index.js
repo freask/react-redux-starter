fe98b4f5151d6a4d39ed231ad6009d14708ffa38
= <=>
2019-02-07 13:47:33 +0300

    adsf

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import { App } from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+
+
+
 import './styles/app.scss';
 
 ReactDOM.render(<App />, document.getElementById('app'));
